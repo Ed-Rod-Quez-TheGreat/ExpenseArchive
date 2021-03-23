3a103c5413eb7bf28c14e514e929d41fc048070b
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2021-03-22 21:09:30 -0600

    first commit'

---
+++ src/main/java/com/EdRodriquez/ExpenseArchive/ExpenseArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.EdRodriquez.ExpenseArchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseArchiveApplication.class, args);
+	}
+
+}
