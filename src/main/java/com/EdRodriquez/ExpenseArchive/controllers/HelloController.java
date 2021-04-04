f8e1717787bdece22b9ca6c67d4cb2db78f2bcfd
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2021-04-04 18:42:35 -0500

    added basic controller for 'all expenses' directory

--- src/main/java/com/EdRodriquez/ExpenseArchive/controllers/HelloController.java
+++ src/main/java/com/EdRodriquez/ExpenseArchive/controllers/HelloController.java
@@ -8,9 +8,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @ResponseBody //responds with plain text, no templates
-    @GetMapping //only handles GET HTTP requests
+    @GetMapping //only handles GET HTTP requests at root path
     public String hello() {
         return "Hello, Archive";
     }
+
+    @ResponseBody
+    @GetMapping("hello") // handles requests at website rootpath/hello
+    public String helloAgain() {
+        return "Hello Again Archive";
+    }
+
+    @ResponseBody
+    @GetMapping("allexpenses") // handles requests at website rootpath/allexpenses
+    public String allUserExpenses() {
+        return "Here are all your monthly expenses:";
+    }
+
+
 
 }
